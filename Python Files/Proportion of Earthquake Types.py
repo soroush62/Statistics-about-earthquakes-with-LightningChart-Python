e5189811f7d2a4828dce35f30c91da51a8544546
soroush62 <112301194+soroush62@users.noreply.github.com>
2024-07-22 09:43:50 +0300

    finalizing creating pie chart

---
+++ Python Files/Proportion of Earthquake Types.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import lightningchart as lc
+import seaborn as sns
+
+# Set your LightningChart license key
+with open('D:/Computer Aplication/WorkPlacement/Projects/shared_variable.txt', 'r') as f:
+    mylicensekey = f.read().strip()
+lc.set_license(mylicensekey)
+
+# Load the dataset
+file_path = 'D:/Computer Aplication/WorkPlacement/Projects/Project5/usgs_main.csv'
+data = pd.read_csv(file_path)
+
+# Calculate the proportions of each earthquake type
+mag_type_counts = data['type'].value_counts()
+
+# Prepare the data for LightningChart
+slices_data = [{'name': mag_type, 'value': count} for mag_type, count in mag_type_counts.items()]
+
+# Create the pie chart
+chart = lc.PieChart(
+    labels_inside_slices=False,
+    title='Proportion of Earthquake Types',
+    theme=lc.Themes.White
+)
+
+# Add slices to the pie chart
+chart.add_slices(slices_data)
+
+# Set the inner radius for a donut chart effect (optional, can be set to 0 for a standard pie chart)
+chart.set_inner_radius(50)
+
+# Open the chart in the browser
+chart.open()
